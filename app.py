b7871a8aa8909b1b2a2c25ff79acfa1d9030a843
yoonshid <yoonshid@gmail.com>
2025-03-09 19:03:57 +0900

    new color html

--- app.py
+++ app.py
@@ -1,44 +1,55 @@
-# Import necessary functions and classes from the Flask library.
 from flask import Flask, render_template, request, redirect, url_for
+import os
+from PIL import Image
+import numpy as np
 
-# Create an instance of the Flask class. This instance is our WSGI application.
 app = Flask(__name__)
 
-# Define a route for the homepage ('/').
+# Set the folder where images will be stored
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
+
+# Function to check file extensions
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+# Route for the homepage
 @app.route('/')
-def home():
-    # Render and return the HTML template 'home.html' located in the 'templates' folder.
-    return render_template('home.html')
+def homepage():
+    return render_template('homepage.html')  # This will render the homepage template
 
-# Define a route for the color selection page ('/colors').
-# We allow both GET and POST methods because:
-# - GET is used when first arriving at the page.
-# - POST is used when the user submits the form (i.e., selects a color).
-@app.route('/colors', methods=['GET', 'POST'])
-def colors():
-    # Check if the form on the page was submitted using a POST request.
+# Route for uploading the image
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
     if request.method == 'POST':
-        # Retrieve the value of 'color' from the submitted form data.
-        color = request.form.get('color')
-        # If a color was selected (i.e., the color variable is not empty)
-        if color:
-            # Redirect the user to the 'result' route.
-            # The chosen color is passed as a URL parameter (query string) with the key 'chosen_color'.
-            return redirect(url_for('result', chosen_color=color))
-    # If the request method is GET (or no valid form data was submitted), display the color selection page.
-    return render_template('color.html')
+        # Check if the file part is in the request
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            # Save the file to the upload folder
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
 
-# Define a route for the result page ('/result').
+            # Open the image and calculate the average color
+            img = Image.open(filename)
+            img = img.convert('RGB')
+            img_array = np.array(img)
+            avg_color = img_array.mean(axis=(0, 1))  # Calculate the average color
+            r, g, b = int(avg_color[0]), int(avg_color[1]), int(avg_color[2])
+
+            # Redirect to result page with RGB values and filename
+            return redirect(url_for('result', r=r, g=g, b=b, filename=file.filename))
+
+    return render_template('upload.html')
+
+# Route for displaying the result
 @app.route('/result')
 def result():
-    # Retrieve the 'chosen_color' parameter from the URL query string.
-    # If it doesn't exist, default to the string 'None'.
-    chosen_color = request.args.get('chosen_color', 'None')
-    # Render the 'result.html' template, passing the chosen color as a variable called 'color'
-    # so it can be used in the HTML template.
-    return render_template('result.html', color=chosen_color)
+    r = int(request.args.get('r'))
+    g = int(request.args.get('g'))
+    b = int(request.args.get('b'))
+    filename = request.args.get('filename')
 
-# This conditional ensures the Flask app runs only if this script is executed directly.
-# 'debug=True' enables debug mode, which is useful during development.
+    return render_template('result.html', r=r, g=g, b=b, filename=filename)
+
 if __name__ == '__main__':
     app.run(debug=True)
